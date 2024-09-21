ac850d1ae4eef1e315c84cdc9a1d1f3109e49dec
Margarita827 <margaiba5@gmail.com>
2024-09-21 11:28:30 +0100

    html corrected

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
 let instrucionsInput = document.querySelector("#user-instructions");
 let apiKey = "o84c2a432ct7ef09733306be2507e42f";
 let prompt = `User instructions: Generate a Spanish poem about ${instrucionsInput.value}`;
-let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a four line poem in html.Please as a <br/> between lines. Make sure to follow the user instructions";
+let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a four line poem. Please add a <br/> between lines. Make sure to follow the user instructions";
 
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
